b7f49afa9d66a10bed4b64f899b75c7a1ee8e9ba
CrownlineProperties <crownlinep@gmail.com>
2025-06-10 05:00:48 +0100

    Updated AreaGuidePage.tsx

--- src/pages/AreaGuidePage.tsx
+++ src/pages/AreaGuidePage.tsx
@@ -78,7 +78,7 @@ const AreaGuidePage = () => {
       <Hero
         title="London Area Guide"
         subtitle="Discover the unique character and investment potential of London's most sought-after neighborhoods"
-        backgroundImage="https://images.pexels.com/photos/427679/pexels-photo-427679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        backgroundImage="https://images.unsplash.com/photo-1529655683826-aba9b3e77383?q=80&w=3165&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         fullHeight={false}
       />
 
