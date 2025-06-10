12eddc8e73d484e2170a74c88d4dfe786d9cafe0
CrownlineProperties <crownlinep@gmail.com>
2025-06-10 18:59:03 +0100

    Updated DataProtectionPage.tsx

--- src/pages/DataProtectionPage.tsx
+++ src/pages/DataProtectionPage.tsx
@@ -5,7 +5,7 @@ const DataProtectionPage = () => {
   return (
     <div>
       <Hero
-        title="Cookie Policy"
+        title="Data Protection"
         subtitle="How we use cookies and similar tracking technologies on our website"
         backgroundImage="https://images.pexels.com/photos/5668473/pexels-photo-5668473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         fullHeight={false}
